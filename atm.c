81174087b0396df03442e07512bfab40d657fd85
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-04-07 22:00:09 +0530

    added threshold for max possible amount
    if more than max possible value(i.e. 31999) is given as input, then it
    will give 0x0

--- atm.c
+++ atm.c
@@ -8,7 +8,7 @@ unsigned int get_money(unsigned short int amount)
   int_array denominations = {1, 5, 10, 20, 50, 100, 500, 2000};
   unsigned int dispense_notes = 0x0;
 
-  for (int index = 7; index >= 0; index--)
+  for (int index = 7; index >= 0 && amount <= 31999; index--)
   {
     dispense_notes |= (amount / denominations[index]) << (index * 4);
     amount %= denominations[index];
