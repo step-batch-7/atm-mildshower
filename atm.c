7be0d690f9226a74008ffe74e2386cf932b46f37
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-04-07 23:40:46 +0530

    refactored both functios

--- atm.c
+++ atm.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include "atm.h"
 
-typedef int int_array[];
-
 unsigned int get_money(unsigned short int amount)
 {
-  int_array denominations = {1, 5, 10, 20, 50, 100, 500, 2000};
+  int_array denominations = denominations_list;
   unsigned int dispense_notes = 0x0;
 
-  for (int index = 7; index >= 0 && amount <= 31999; index--)
+  if (amount > 31999)
+    return dispense_notes;
+
+  for (int index = 0; index < 8; index++)
   {
-    dispense_notes |= (amount / denominations[index]) << (index * 4);
+    dispense_notes <<= 4;
+    dispense_notes |= amount / denominations[index];
     amount %= denominations[index];
   }
 
@@ -19,13 +21,14 @@ unsigned int get_money(unsigned short int amount)
 
 void display_notes(unsigned int notes)
 {
-  int_array denominations = {1, 5, 10, 20, 50, 100, 500, 2000};
+  int_array denominations = denominations_list;
   int notes_count;
 
-  for (int index = 7; index >= 0; index--)
+  for (int index = 0; index < 8; index++)
   {
-    notes_count = notes >> (index * 4) & 0xf;
+    notes_count = (notes & 0xf0000000) >> 28;
     if (notes_count)
-      printf("%d note(s) of Rs %d\n", notes_count, denominations[index]);
+      printf("%d %s of Rs %d\n", notes_count, label(notes_count), denominations[index]);
+    notes <<= 4;
   }
 }
--- atm.h
+++ atm.h
@@ -18,5 +18,11 @@
    2590 0x11012000
   31999 0xf3412014
 */
+
+#define denominations_list  {2000, 500, 100, 50, 20, 10, 5, 1}
+#define label(count) count > 1 ? "notes" : "note"
+
+typedef int int_array[];
+
 unsigned int get_money(unsigned short int);
 void display_notes(unsigned int);
